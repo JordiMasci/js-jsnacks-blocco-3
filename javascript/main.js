860d6d20e43b92c4554e619b28b922601b6e9ca5
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-07-26 17:26:39 +0200

    feat: esercizio 5

--- javascript/main.js
+++ javascript/main.js
@@ -37,9 +37,6 @@
 //   alert(firstWord + " " + secondWord);
 // }
 
-
-
-
 // TERZO/QUARTO ESERCIZIO
 
 // crea array vuoto
@@ -59,3 +56,20 @@
 
 // userNumberMedia = (number / numbers.length);
 // console.log(userNumberMedia);
+
+// QUINTO ESERCIZIO
+// Trovare gli errori nel seguente codice:
+
+// --------------------------
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+let evenNumbers = [];
+for (let i = 0; i < numbers.length - 1; i++) {
+  if (numbers[i] % 2 == 0)
+  {
+    evenNumbers.push(i);
+  }
+}
+console.log(evenNumbers);
+
+// dovrebbe restituire [1,3,5]
+// --------------------------
