738c921ba6227247b458b43a9b8541ba21a345f8
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-07-26 16:07:49 +0200

    feat: esercizio 2

--- javascript/main.js
+++ javascript/main.js
@@ -1,17 +1,42 @@
-// chiedi all'utente 1 numero
-const firstNumber = parseInt(prompt("inserisci un numero"));
+// PRIMO ESERCIZIO
 
-// chiedi all'utente un secondo numero
-const secondNumber = parseInt(prompt("inserisci un numero"));
 
-let randomNumber;
-// genera un numero casuale compreso tra i due
-if (firstNumber > secondNumber) {
-  randomNumber = Math.floor(Math.random() * (firstNumber - secondNumber + 1)) + secondNumber;
-} else if(secondNumber > firstNumber) {
-  randomNumber = Math.floor(Math.random() * (secondNumber - firstNumber + 1)) + firstNumber;
+// // chiedi all'utente 1 numero
+// const firstNumber = parseInt(prompt("inserisci un numero"));
+
+// // chiedi all'utente un secondo numero
+// const secondNumber = parseInt(prompt("inserisci un numero"));
+
+// let randomNumber;
+// // genera un numero casuale compreso tra i due
+// if (firstNumber > secondNumber) {
+//   randomNumber = Math.floor(Math.random() * (firstNumber - secondNumber + 1)) + secondNumber;
+// } else if(secondNumber > firstNumber) {
+//   randomNumber = Math.floor(Math.random() * (secondNumber - firstNumber + 1)) + firstNumber;
+// } else {
+//     alert('i numeri sono uguali');
+// }
+
+//console.log(randomNumber);
+
+
+
+// SECONDO ESERCIZIO
+
+// chiedere 1 parola
+const firstWord = prompt("inserisci una parola");
+
+// chiedere 2 parola
+const secondWord = prompt('inserisci un"altra parola');
+// verificare la lunghezza delle due parole
+
+
+// stampare la parola più lunga
+if (firstWord.length > secondWord.length) {
+    alert(firstWord);
+} else if (firstWord.length < secondWord.length) {
+    alert(secondWord);
 } else {
-    alert('i numeri sono uguali');
+    // se uguali stamparle entrambe
+  alert(firstWord + " " + secondWord);
 }
-
-console.log(randomNumber);
--- readme.md
+++ readme.md
@@ -4,3 +4,11 @@
 - chiedi all'utente 1 numero
 - secondo numero
 - genera un numero casuale compreso tra i due
+
+
+# SVOLGIMENTO 2
+- chiedere 1 parola
+- chiedere 2 parola
+- verificare la lunghezza delle due parole
+- stampare la parola più lunga
+- se uguali stamparle entrambe
