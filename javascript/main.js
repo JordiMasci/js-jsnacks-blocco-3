ba5f1df2bf1240b8d20a4acd4e29f6e90151cf4a
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-07-26 16:50:03 +0200

    feat: terzo e quarto esercizio

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,5 @@
 // PRIMO ESERCIZIO
 
-
 // // chiedi all'utente 1 numero
 // const firstNumber = parseInt(prompt("inserisci un numero"));
 
@@ -19,24 +18,41 @@
 
 //console.log(randomNumber);
 
-
-
 // SECONDO ESERCIZIO
 
-// chiedere 1 parola
-const firstWord = prompt("inserisci una parola");
+// // chiedere 1 parola
+// const firstWord = prompt("inserisci una parola");
 
-// chiedere 2 parola
-const secondWord = prompt('inserisci un"altra parola');
-// verificare la lunghezza delle due parole
+// // chiedere 2 parola
+// const secondWord = prompt('inserisci un"altra parola');
+// // verificare la lunghezza delle due parole
 
+// // stampare la parola più lunga
+// if (firstWord.length > secondWord.length) {
+//     alert(firstWord);
+// } else if (firstWord.length < secondWord.length) {
+//     alert(secondWord);
+// } else {
+//     // se uguali stamparle entrambe
+//   alert(firstWord + " " + secondWord);
+// }
 
-// stampare la parola più lunga
-if (firstWord.length > secondWord.length) {
-    alert(firstWord);
-} else if (firstWord.length < secondWord.length) {
-    alert(secondWord);
-} else {
-    // se uguali stamparle entrambe
-  alert(firstWord + " " + secondWord);
+// TERZO ESERCIZIO
+
+// crea array vuoto
+const numbers = [];
+
+// chiedi all'utente un numero
+let number = 0;
+
+// inserire un numero nell'array
+while (number < 50) {
+  // chiedere fino a quando non è minore di 50
+  let userNumber = parseInt(prompt("Inserisci un numero"));
+  number += userNumber;
+  numbers.push(userNumber);
 }
+console.log(numbers);
+
+userNumberMedia = (numbers / numbers.length);
+console.log(userNumberMedia);
--- readme.md
+++ readme.md
@@ -12,3 +12,9 @@
 - verificare la lunghezza delle due parole
 - stampare la parola più lunga
 - se uguali stamparle entrambe
+
+# SVOLGIMENTO 3
+- crea array vuoto 
+- chiedi all'utente un numero
+- inserire un numero nell'array
+- continua a chiedere numeri all'utente fino a quando la somma non è minore di 50
